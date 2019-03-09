cedf06b32bb5a9d074c44f9d1388fbab4351799c
Benjamin Thominet <bthomine@utbm.fr>
2019-03-09 11:42:20 +0100

    new data abse product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_081443) do
+ActiveRecord::Schema.define(version: 2019_03_09_102139) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "price"
+    t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# products.create(name: 'Chameau',  description: 'Sympathique compagnon', price: 600, picture: 'photo')
+# products.create(name: 'Camel',  description: 'Compagnon a fumer', price: 15, picture: 'photo')
+# products.create(name: 'Mon cheri',  description: 'Fort goutu', price: 10, picture: 'photo')
+# products.create(name: 'Patate incurvée',  description: 'Fort goutu, mais incurvée', price: 5, picture: 'photo')
