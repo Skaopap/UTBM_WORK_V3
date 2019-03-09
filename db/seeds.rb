d3bc1cd616dab31fc7546efe526195ada07c0668
Benjamin Thominet <bthomine@utbm.fr>
2019-03-09 12:09:25 +0100

    seed and a bit of css lol nope

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# products.create(name: 'Chameau',  description: 'Sympathique compagnon', price: 600, picture: 'photo')
-# products.create(name: 'Camel',  description: 'Compagnon a fumer', price: 15, picture: 'photo')
-# products.create(name: 'Mon cheri',  description: 'Fort goutu', price: 10, picture: 'photo')
-# products.create(name: 'Patate incurvée',  description: 'Fort goutu, mais incurvée', price: 5, picture: 'photo')
+Product.create(name: 'Chameau',  description: 'Sympathique compagnon', price: 600, picture: 'https://fr.cdn.v5.futura-sciences.com/buildsv6/images/wide1920/8/5/4/854c15a2cc_100124_chameau-dromadaire-difference-01.jpg')
+Product.create(name: 'Camel',  description: 'Compagnon a fumer', price: 15, picture: 'http://m.memegen.com/ocsd6p.jpg')
+Product.create(name: 'Mon cheri',  description: 'Fort goutu', price: 10, picture: 'https://www.dictionary.com/e/wp-content/uploads/2018/10/mon-cheri-2.jpg')
+Product.create(name: 'Patate incurvée',  description: 'Fort goutu, mais incurvée', price: 5, picture: 'https://www.jardindorante.fr/468-large_default/decembre-2017-patate-douce.jpg')
